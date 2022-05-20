0dd500823c9880b1d24d3d39dc751c4b80ad0638
RyanDBarnett <conner2468@gmail.com>
2022-05-20 10:56:28 -0600

    Create Pirate and add name attribute

---
+++ mythical-creatures/exercises/pirate.js
@@ -0,0 +1,7 @@
+class Pirate {
+  constructor(name) {
+    this.name = name;
+  }
+}
+
+module.exports = Pirate;
--- mythical-creatures/test/pirate-test.js
+++ mythical-creatures/test/pirate-test.js
@@ -3,17 +3,17 @@ var Pirate = require('../exercises/pirate');
 
 describe('Pirate', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     assert.isFunction(Pirate);
   });
 
-  it.skip('should instantiate our good friend, Pirate', function() {
+  it('should instantiate our good friend, Pirate', function() {
     var dreadPirateRoberts = new Pirate();
 
     assert.instanceOf(dreadPirateRoberts, Pirate);
   });
 
-  it.skip('should have a name', function() {
+  it('should have a name', function() {
     var blackbeard = new Pirate('Blackbeard');
     var chengISao = new Pirate('Cheng I Sao');
 
