85557476888198bb9af7e8c8aee151cb50db7929
RyanDBarnett <conner2468@gmail.com>
2022-05-20 10:21:10 -0600

    Create Dragon class

--- mythical-creatures/test/dragon-test.js
+++ mythical-creatures/test/dragon-test.js
@@ -3,11 +3,11 @@ var Dragon = require('../exercises/dragon');
 
 describe('Dragon', function() {
 
-  it.skip('should be a function', function() {
+  it('should be a function', function() {
     assert.isFunction(Dragon);
   });
 
-  it.skip('should instantiate our good friend, Dragon', function() {
+  it('should instantiate our good friend, Dragon', function() {
     var smaug = new Dragon();
 
     assert.instanceOf(smaug, Dragon);
@@ -44,15 +44,15 @@ describe('Dragon', function() {
     var dragon = new Dragon('Lady Vox');
 
     dragon.eat();
-    
-    assert.equal(dragon.hungry, true);
-    
-    dragon.eat();
-    
+
     assert.equal(dragon.hungry, true);
 
     dragon.eat();
-    
+
+    assert.equal(dragon.hungry, true);
+
+    dragon.eat();
+
     assert.equal(dragon.hungry, false);
   });
 });
