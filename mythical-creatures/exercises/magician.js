3bdf17ab389ce84590b6894866695cb6861e7fa3
RyanDBarnett <conner2468@gmail.com>
2022-05-23 09:10:36 -0600

    Create Magician Class

---
+++ mythical-creatures/exercises/magician.js
@@ -0,0 +1,5 @@
+class Magician {
+
+}
+
+module.exports = Magician;
--- mythical-creatures/test/magician-test.js
+++ mythical-creatures/test/magician-test.js
@@ -3,7 +3,7 @@ var Magician = require('../exercises/magician');
 
 describe('Magician', function() {
 
-  it.skip('should be a function that instantiates a class', function() {
+  it('should be a function that instantiates a class', function() {
     assert.isFunction(Magician);
     assert.instanceOf(new Magician(), Magician);
   });
@@ -30,36 +30,36 @@ describe('Magician', function() {
 
   it.skip('should be able to cast a spell', function() {
     var magician = new Magician({ name: 'Scott', topHat: false });
-    
+
     var spell = magician.incantation('abracadabra');
     var charm = magician.incantation('allakazaam')
-    
+
     assert.equal(spell, 'ABRACADABRA!');
     assert.equal(charm, 'ALLAKAZAAM!');
   });
-  
+
   it.skip('should be able to pull a rabbit from the hat, if the magician has a top hat', function() {
     var magician = new Magician({ name: 'Casey' });
-    
+
     var spell = magician.cast();
-    
+
     assert.equal(spell, 'PULL RABBIT FROM TOP HAT');
   });
-  
+
   it.skip('should be able to pull a dove from the sleeve, if the magician does not have a top hat', function() {
     var magician = new Magician({ name: 'Leta', topHat: false });
-    
+
     var spell = magician.cast();
-    
+
     assert.equal(spell, 'PULL DOVE FROM SLEEVE');
   });
-  
+
   it.skip('should start not confident', function() {
     var magician = new Magician({ name: 'Leta', topHat: false });
-    
+
     assert.equal(magician.confident, false);
   });
-  
+
   it.skip('should become confident after casting three spells', function() {
     var magician = new Magician({ name: 'Leta', topHat: false });
 
