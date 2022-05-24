ba640c8b5ede43b8b6223b2fa6086463669e3a7d
RyanDBarnett <conner2468@gmail.com>
2022-05-24 11:47:07 -0600

    Sphinx mocks heros when they get correct answer

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -15,6 +15,7 @@ class Sphinx {
     for (let i = 0; i < this.riddles.length; i++) {
       if (this.riddles[i].answer == answerAttempt) {
         this.riddles.splice(i, 1);
+        return 'That wasn\'t that hard, I bet you don\'t get the next one';
       }
     }
   }
