d159464e63767e2894d45e13f6ae7270140574a4
RyanDBarnett <conner2468@gmail.com>
2022-05-24 10:14:46 -0600

    Fairy becomes vengeful when provoked

--- mythical-creatures/exercises/fairy.js
+++ mythical-creatures/exercises/fairy.js
@@ -6,16 +6,20 @@ class Fairy {
     this.clothes = {dresses: ['Iris']};
   }
 
-  receiveBelief() {
-    this.dust++;
-  }
-
   believe() {
     this.dust += 10;
   }
 
   makeDresses(dresses) {
     this.clothes.dresses = this.clothes.dresses.concat(dresses);
+  }
+
+  provoke() {
+    this.disposition = 'Vengeful';
+  }
+
+  receiveBelief() {
+    this.dust++;
   }
 }
 
